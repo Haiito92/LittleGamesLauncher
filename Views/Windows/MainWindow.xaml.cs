6d8d419b40f9c57c73c8074b92ae49485bace05c
Haiito92 <116373874+Haiito92@users.noreply.github.com>
2025-05-20 22:53:06 +0200

    First TitleBar customisation

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using Windows.UI;
+using Windows.Graphics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,6 +17,58 @@ namespace LittleGamesLauncher.Views.Windows
         public MainWindow()
         {
             this.InitializeComponent();
+
+            if (AppWindow.Presenter is not OverlappedPresenter overlappedPresenter) return;
+
+            AppWindow.Resize(new SizeInt32(1600,900));
+
+            overlappedPresenter.IsMaximizable = false;
+            overlappedPresenter.IsResizable = false;
+
+            ExtendsContentIntoTitleBar = true;
+
+            SetTitleBarColors();
+
+            if (Content is FrameworkElement rootElement)
+            {
+                rootElement.ActualThemeChanged += OnThemeChanged;
+            }
         }
-    }
+
+        private void SetTitleBarColors()
+        {
+            AppWindowTitleBar titleBar = AppWindow.TitleBar;
+
+            if (titleBar == null) return;
+
+            if (Application.Current.Resources.TryGetValue("SecondaryColor", out object secondaryColor))
+            {
+                titleBar.ButtonBackgroundColor = (Color)secondaryColor;
+            }
+
+            if (Application.Current.Resources.TryGetValue("HoverSecondaryColor", out object hoverSecondaryColor))
+            {
+                titleBar.ButtonHoverBackgroundColor = (Color)hoverSecondaryColor;
+            }
+
+            if (Application.Current.Resources.TryGetValue("PressedSecondaryColor", out object pressedSecondaryColor))
+            {
+                titleBar.ButtonPressedBackgroundColor = (Color)pressedSecondaryColor;
+            }
+
+            if (Application.Current.Resources.TryGetValue("NeutralColor", out object neutralColor))
+            {
+                titleBar.ButtonForegroundColor = (Color)neutralColor;
+                titleBar.ButtonHoverForegroundColor = (Color)neutralColor;
+                titleBar.ButtonPressedForegroundColor = (Color)neutralColor;
+            }
+
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        private void OnThemeChanged(FrameworkElement sender, object args)
+        {
+            SetTitleBarColors();
+        }
+    }   
 }
